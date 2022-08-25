49a6d52fb8c5cdff736785cc8d7f70b445338723
Adewale Adeyinka <adeyinkawale13@gmail.com>
2022-08-25 08:28:04 +0100

    "added expensechart and chartbar"

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-
-import './App.css';
-import Movie from './components/Movie';
-import movies from './data/movielist';
+import Expenses from './components/expenses/Expenses';
+import NewExpense from './components/newExpense/NewExpense';
+import React, { useState } from 'react';
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
+ const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
-  function displayMovie(movies){
-    return (<Movie
-        key = {movies.id}
-        name = {movies.name}
-        image = {movies.image}
-        year = {movies.year}
-        box_office = {movies.box_office}
-    />
-    )
-  }
+  const addExpenseHandler = (expense) => {
+   setExpenses(prevExpenses =>{
+     return [expense, ...prevExpenses]
+   });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
-    <div className="App">
-      <h1 className='App-header'>Top 10 Highest-Grossing Movies of all Times</h1>
-      <div className='movie-list'>
-        {movies.map(displayMovie)}
-      </div>
-          
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
